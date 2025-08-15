4f5c0fe7e3c11817e14078cc4081c474731736a2
NishantBansal-877 <nishant87706@gmail.com>
2025-08-15 18:20:57 +0530

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -3,27 +3,48 @@ import { useState } from "react";
 import { ImageUploadSection } from "./imageUploadSection";
 import { Alert } from "./alert";
 import { ShowSelectedImages } from "./selectedImages";
-import { LoginAndSignup } from "./loginAndSignup";
 import Pricing from "./pricing";
 import OtpPage from "./otpPage";
 import { Preview } from "./preview";
 import LoadingOverlay from "./loadingOverlay";
 import { ForgetPasswordPage } from "./forgetPasswordPage";
+import Login from "./login";
+import Signup from "./signup";
 
-export function Hero({ view, setView, setUser, setLogout, email, setEmail }) {
+export function Hero({ view, setView, setUser, setLogout, email, setEmail, setShowNotif, setNotifyMsg, selectedImages, setSelectedImages }) {
   const [quality, setQuality] = useState(10);
-  const [selectedImages, setSelectedImages] = useState([]);
   const [previewImages, setPreviewImages] = useState({});
   const [message, setMessage] = useState(null);
   const heroViews = {
     alert: <Alert setView={setView} />,
-    auth: <LoginAndSignup setView={setView} setLogout={setLogout} setUser={setUser} setEmail={setEmail} setMessage={setMessage} />,
-    forget: <ForgetPasswordPage setView={setView} email={email} setEmail={setEmail} setMessage={setMessage} />,
+    // auth: (
+    //   <LoginAndSignup
+    //     setView={setView}
+    //     setLogout={setLogout}
+    //     setUser={setUser}
+    //     setEmail={setEmail}
+    //     setMessage={setMessage}
+    //     setShowNotif={setShowNotif}
+    //     setNotifyMsg={setNotifyMsg}
+    //   />
+    // ),
+    forget: (
+      <ForgetPasswordPage
+        setView={setView}
+        email={email}
+        setEmail={setEmail}
+        setMessage={setMessage}
+        setShowNotif={setShowNotif}
+        setNotifyMsg={setNotifyMsg}
+      />
+    ),
     home: <ImageUploadSection setView={setView} setSelectedImages={setSelectedImages} />,
     loading: <LoadingOverlay />,
-    otpPage: <OtpPage email={email} setView={setView} message={message} />,
+    login: <Login setLogout={setLogout} setNotifyMsg={setNotifyMsg} setShowNotif={setShowNotif} setUser={setUser} setView={setView} />,
+    otpPage: <OtpPage email={email} setView={setView} message={message} setShowNotif={setShowNotif} setNotifyMsg={setNotifyMsg} />,
     pricing: <Pricing />,
     preview: <Preview previewImages={previewImages} />,
+    signup: <Signup setEmail={setEmail} setMessage={setMessage} setNotifyMsg={setNotifyMsg} setShowNotif={setShowNotif} setView={setView} />,
     selectedImages: (
       <ShowSelectedImages
         quality={quality}
@@ -33,6 +54,8 @@ export function Hero({ view, setView, setUser, setLogout, email, setEmail }) {
         setView={setView}
         setLogout={setLogout}
         setPreviewImages={setPreviewImages}
+        setShowNotif={setShowNotif}
+        setNotifyMsg={setNotifyMsg}
       />
     ),
   };
