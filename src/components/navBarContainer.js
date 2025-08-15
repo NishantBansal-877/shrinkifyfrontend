8f2e91f93a5dacd38e6cf673391edc305ef668f4
NishantBansal-877 <nishant87706@gmail.com>
2025-08-15 18:23:46 +0530

    Update navBarContainer.js

--- src/components/navBarContainer.js
+++ src/components/navBarContainer.js
@@ -1,23 +1,36 @@
-export function NavbarContainer({ setView, user, logout, setLogout }) {
+export function NavbarContainer({ setView, user, logout, setLogout, setShowNotif, setNotifyMsg, setSelectedImages }) {
   return (
     <div className="navbar-container">
-      <span className="logo">⚡ SHRINKER</span>
+      <span
+        className="logo"
+        onClick={() => {
+          setSelectedImages([]);
+          setView("home");
+        }}
+      >
+        ⚡ SHRINKER
+      </span>
       {logout && (
         <span className="welcomeText">
           👋 Welcome back, <strong>{user.fullName}</strong>!
         </span>
       )}
       <div className="nav-links">
-        <a href="#hero" onClick={() => setView("home")}>
+        <a
+          href="#hero"
+          onClick={() => {
+            setSelectedImages([]);
+            setView("home");
+          }}
+        >
           Home
         </a>
-      
-        <a href="#hero">
+        {/* <a href="#hero" onClick={() => setView("pricing")}>
           Pricing
-        </a>
-        <a href="#contact">Contacts</a>
+        </a> */}
+        {logout && <a href="#contact">Contacts</a>}
         {!logout ? (
-          <a href="#hero" onClick={() => setView("auth")}>
+          <a href="#hero" onClick={() => setView("login")}>
             Login/SignUp
           </a>
         ) : (
@@ -28,12 +41,13 @@ export function NavbarContainer({ setView, user, logout, setLogout }) {
                 method: "GET",
                 credentials: "include",
               });
-
-              // const data = await result.text();
-              // alert(data);
+              const data = await result.text();
+              setSelectedImages([]);
+              setShowNotif(true);
+              setNotifyMsg(data);
               setLogout(false);
               setView("loading");
-              setTimeout(() => setView("auth"), 2000);
+              setTimeout(() => setView("login"), 2000);
             }}
           >
             Logout
