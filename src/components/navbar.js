19b8de8fca9f4a41a1b2c14f9a1cbdb5966c8eda
NishantBansal-877 <nishant87706@gmail.com>
2025-08-15 18:23:13 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,17 @@
 import { NavbarContainer } from "./navBarContainer";
 
-export function Navbar({ setView, user, logout, setLogout }) {
+export function Navbar({ setView, user, logout, setLogout, setShowNotif, setNotifyMsg, setSelectedImages }) {
   return (
-    <nav className="navbar" id="navbar">
-      <NavbarContainer setView={setView} user={user} logout={logout} setLogout={setLogout} />
+    <nav className="navbar" id="navbar" style={{ zIndex: "5", position: "sticky", width: "100%", top: "0" }}>
+      <NavbarContainer
+        setView={setView}
+        user={user}
+        logout={logout}
+        setLogout={setLogout}
+        setShowNotif={setShowNotif}
+        setNotifyMsg={setNotifyMsg}
+        setSelectedImages={setSelectedImages}
+      />
     </nav>
   );
 }
